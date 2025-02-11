b142da308f7b1a81e207736072a08456590ffcfb
tnantaki <tinyhave@gmail.com>
2025-02-11 14:57:19 +0700

    config vite: update base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/j_travel-react"
+  base: "/j_travel-react/"
 })
