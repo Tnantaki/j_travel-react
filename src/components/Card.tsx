a5301ad471c9907daaf0d75d3ba966fd45e99bac
tnantaki <tinyhave@gmail.com>
2025-02-14 11:23:44 +0700

    implement responsive in packages page

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,11 +6,11 @@ interface Props {
 
 const Card = ({ imgPath, title, description }: Props) => {
   return (
-    <div className="flex flex-col w-[400px] rounded-xl overflow-hidden items-center bg-dark-secondary">
-      <div className="h-[300px] w-full overflow-hidden">
+    <div className="flex flex-col max-w-[400px] rounded-xl overflow-hidden items-center bg-dark-secondary">
+      <div className="aspect-4/3 w-full overflow-hidden">
         <img src={imgPath} alt="location image" className="object-center" />
       </div>
-      <div className="p-6 grid gap-4">
+      <div className="p-3 grid gap-2 sm:p-6 sm:gap-4">
         <h4 className="text-center">{title}</h4>
         <p>{description}</p>
       </div>
--- src/routes/packages/Packages.tsx
+++ src/routes/packages/Packages.tsx
@@ -23,17 +23,17 @@ const Packages = () => {
   ];
 
   return (
-    <section className="relative min-h-screen flex overflow-hidden bg-dark-primary justify-center hero">
-      <div className="flex flex-col mt-20 mx-40 items-center gap-12">
-        <div className="flex flex-col items-center gap-10">
+    <section className="bg-dark-primary justify-center hero sec-padding">
+      <div className="page-container mt-5 flex flex-col items-center gap-12 sm:mt-10">
+        <div className="flex flex-col items-center gap-2 lg:gap-10 text-center">
           <h2>Discover Our Top Japan Tour Packages</h2>
-          <p className="text-xl font-normal text-center px-40">
+          <p className="text-base lg:text-xl">
             Experience the beauty and culture of Japan with our curated tour
             packages. Each journey is designed to immerse you in the unique
             landscapes and traditions of this incredible country.
           </p>
         </div>
-        <div className="grid grid-cols-3 gap-16">
+        <div className="grid gap-6 sm:grid-cols-2 sm:gap-8 lg:grid-cols-3 lg:gap-12">
           {packages.map((p, i) => (
             <Card key={i} {...p} />
           ))}
