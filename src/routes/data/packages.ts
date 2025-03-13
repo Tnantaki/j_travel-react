046b5d0838b622420db4ff086a8a913aa378feb2
tnantaki <tinyhave@gmail.com>
2025-03-13 15:52:50 +0700

    update select package

--- src/routes/data/packages.ts
+++ src/routes/data/packages.ts
@@ -4,18 +4,21 @@ import kinkakuji from "@img/location/kinkakuji-temple-kyoto.png";
 
 const packages = [
   {
+    id: "osaka",
     imgPath: osaka,
     title: "Osaka Castle, Osaka",
     description:
       "a symbol of Japan’s feudal past and a must-visit historical site. Originally built in the late 16th century by Toyotomi Hideyoshi.",
   },
   {
+    id: "kinkakuji",
     imgPath: kinkakuji,
     title: "Kinkaku-ji Temple (Golden Pavilion), Kyoto",
     description:
       "one of Kyoto’s most iconic landmarks. This Zen Buddhist temple is covered in gold leaf, reflecting beautifully in the surrounding Kyoko-chi (Mirror Pond).",
   },
   {
+    id: "senso",
     imgPath: senso,
     title: "Senso-ji Temple, Tokyo",
     description:
@@ -23,4 +26,4 @@ const packages = [
   },
 ];
 
-export default packages
+export default packages;
