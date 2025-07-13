627187f8a709501fb55790143bc3597714cf192f
tnantaki <tinyhave@gmail.com>
2025-07-13 15:42:48 +0700

    add feature: implement real-time search on Package page

--- frontend/src/routes/package/SearchPlan.tsx
+++ frontend/src/routes/package/SearchPlan.tsx
@@ -10,13 +10,16 @@ const SearchPlan = ({ onSearch }: Props) => {
 
   return (
     <div className="flex rounded-full text-char-sec items-center body1 overflow-hidden">
-      <div className="flex w-full bg-frame-ter items-center focus-within:inset-ring-1 ring-pri-shade rounded-s-full">
+      <div className="flex w-full bg-frame-ter items-center focus-within:inset-ring-2 inset-ring-slate-700 rounded-s-full">
         <IoIosSearch className="px-3 w-15 size-12" />
         <input
           type="text"
           placeholder="Search Package..."
           className="py-2 w-full focus:border-0 focus:outline-0"
           ref={textRef}
+          onChange={(e) => {
+            onSearch(e.target.value);
+          }}
         />
       </div>
       <button
