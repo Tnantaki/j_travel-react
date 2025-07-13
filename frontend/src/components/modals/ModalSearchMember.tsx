b34038bd9f9d901919e27cebf683496b17cc43a5
tnantaki <tinyhave@gmail.com>
2025-07-13 15:23:07 +0700

    add feature: implement real-time search members

--- frontend/src/components/modals/ModalSearchMember.tsx
+++ frontend/src/components/modals/ModalSearchMember.tsx
@@ -1,7 +1,7 @@
 import Modal from "./Modal";
 import Button from "../common/Button";
 import { IoIosSearch } from "react-icons/io";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { SearchedMemberType, useBooking } from "../../contexts/BookingProvider";
 import { FaExclamationCircle } from "react-icons/fa";
 import groupService from "../../services/group-service";
@@ -13,9 +13,42 @@ interface Props {
 
 const ModalSearchMember = ({ isOpen, onClose }: Props) => {
   const [members, setMembers] = useState<SearchedMemberType[]>([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [typingTimeout, setTypingTimeout] = useState<number | undefined>();
   const [error, setError] = useState("");
-  const textRef = useRef<HTMLInputElement>(null);
   const { booking, bookDispatch } = useBooking();
+
+  useEffect(() => {
+    if (!searchInput || searchInput.length < 2) {
+      setError("");
+      setMembers([]);
+      return;
+    }
+
+    if (typingTimeout) clearTimeout(typingTimeout);
+
+    const timeOutId = window.setTimeout(() => {
+      groupService
+        .searchMember(searchInput)
+        .then(({ data }) => {
+          const members = data.map((mem) => ({
+            id: mem._id,
+            name: mem.username,
+            email: mem.email,
+          }));
+          setMembers(filterMembers(members));
+        })
+        .catch((err) => {
+          console.log(err);
+          setError("Error");
+          setMembers([]);
+        });
+    }, 300); //debounce time
+
+    setTypingTimeout(timeOutId);
+
+    return () => clearTimeout(timeOutId);
+  }, [searchInput]);
 
   const renderMemberList = (value: string, label: string) => {
     return (
@@ -32,9 +65,7 @@ const ModalSearchMember = ({ isOpen, onClose }: Props) => {
       if (mem.id === booking.leader!.id) {
         return false;
       }
-      const memFilter = booking.members.filter(
-        (memList) => memList.id === mem.id
-      );
+      const memFilter = booking.members.filter((memList) => memList.id === mem.id);
       if (memFilter.length) {
         return false;
       }
@@ -49,11 +80,19 @@ const ModalSearchMember = ({ isOpen, onClose }: Props) => {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    if (textRef.current) {
-      const email = textRef.current.value;
-      try {
-        // search member API
-        const { data } = await groupService.searchMember(email);
+    if (!searchInput || searchInput.length < 2) {
+      setError("Character must be at least 2.");
+      setMembers([]);
+      return;
+    }
+
+    groupService
+      .searchMember(searchInput)
+      .then(({ data }) => {
+        if (Array.isArray(data) && data.length === 0) {
+          setError("Not found");
+          return ;
+        }
 
         const members = data.map((mem) => ({
           id: mem._id,
@@ -62,28 +101,28 @@ const ModalSearchMember = ({ isOpen, onClose }: Props) => {
         }));
 
         setMembers(filterMembers(members));
-      } catch (error) {
+      })
+      .catch((err) => {
+        console.log(err);
         setError("Error");
-      }
-    }
+        setMembers([]);
+      });
   };
 
   return (
     <Modal isOpen={isOpen} hasCloseBtn={true} onClose={onClose}>
       <div className="flex flex-col items-center px-10 py-6 gap-2 bg-linear-light-modal">
         <h3 className="text-char-pri">Search Member</h3>
-        <form
-          className="w-full lg:w-[800px] p-4 flex flex-col items-center"
-          onSubmit={handleSubmit}
-        >
+        <form className="w-full lg:w-[800px] p-4 flex flex-col items-center" onSubmit={handleSubmit}>
           <div className="flex rounded-full text-char-sec items-center body2 overflow-hidden w-full px-4">
             <div className="flex w-full inset-ring-1 inset-ring-frame-ter bg-char-sec text-char-pri items-center focus-within:inset-ring-2 rounded-s-full">
               <IoIosSearch className="px-3 w-15 size-7" />
               <input
-                ref={textRef}
                 type="text"
                 placeholder="Email..."
                 className="py-2 w-full focus:border-0 focus:outline-0"
+                value={searchInput}
+                onChange={(e) => setSearchInput(e.target.value)}
               />
             </div>
             <button
@@ -94,7 +133,7 @@ const ModalSearchMember = ({ isOpen, onClose }: Props) => {
             </button>
           </div>
           {error && (
-            <p className="text-info-error flex items-center gap-1 text-sm mt-0.5 absolute drop-shadow-lg sm:text-base sm:mt-1">
+            <p className="text-info-error flex ps-5 self-start items-center gap-1 text-sm mt-0.5 drop-shadow-lg sm:text-base sm:mt-1">
               <FaExclamationCircle />
               {error}
             </p>
@@ -102,20 +141,12 @@ const ModalSearchMember = ({ isOpen, onClose }: Props) => {
         </form>
         <ul className="lg:h-[500px] w-full flex flex-col gap-2">
           {members.map((mem) => (
-            <li
-              key={mem.id}
-              className="bg-slate-300 border-slate-500 border-1 flex px-6 py-4 rounded-md w-full"
-            >
+            <li key={mem.id} className="bg-slate-300 border-slate-500 border-1 flex px-6 py-4 rounded-md w-full">
               <div className="grid grid-cols-2 w-full gap-2">
                 {renderMemberList(mem.name, "name")}
                 {renderMemberList(mem.email, "email")}
               </div>
-              <Button
-                rounded="full"
-                size="sm"
-                className="self-center"
-                onClick={() => handleAddMember(mem)}
-              >
+              <Button rounded="full" size="sm" className="self-center" onClick={() => handleAddMember(mem)}>
                 Add
               </Button>
             </li>
